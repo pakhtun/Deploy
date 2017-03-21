b7d7b53558a801b0bd28c2379e43fab22a09db42
pakhtun <uk.aamir93@outlook.com>
2017-03-21 10:55:47 -0700

    JSHint and JSCS added

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 var gulp = require('gulp')
 var gutil = require('gulp-util');
-
-var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
  
+gulp.task('jscs', () => {
+    gulp.src('/home/ktuser/Deploy/*.js')
+        .pipe(jscs())
+        .pipe(jscs.reporter('fail'));
+});
+
+
+gulp.task('jshint', function() {
+   gulp.src('/home/ktuser/Deploy/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('fail'));
+});
+
+
+
 gulp.task('test', () =>
-    gulp.src('/home/ktuser/Deploy/test/test.js', {read: false})
-        .pipe(mocha())
+   gulp.src('/home/ktuser/Deploy/test/test.js', {read: false})
+     .pipe(mocha())
 );
 
 
